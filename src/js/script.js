bb9192e61c5c4e23e8bcb76e9f85bfe0647fdbec
CaiozinS2 <caioandersongoes@gmail.com>
2023-12-14 20:00:48 -0300

    Tornando o projeto Responsivo (ou quase) apenas com HTML e CSS

--- src/js/script.js
+++ src/js/script.js
@@ -18,26 +18,20 @@ botoes.forEach(id => {
         imagem.alt = id;
         imagem2.src = `../src/images/back_${id}.jpg`;
 
-        // Ativar o flip-card
-        flipCard.style.pointerEvents = 'auto';
-        // Tornar a div do flip-card visível
+        
+        flipCard.style.pointerEvents = 'auto';     
         flipCard.style.display = 'block';
     })
 });
 
-// Adicionar tratamento para nenhum botão selecionado
 const backgroundPadrao = 'background2.jpg';
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Configurar a imagem de fundo padrão
     container.style.backgroundImage = `url('../src/images/${backgroundPadrao}')`;
 
-    // Configurar a imagem do flip-card desativado
     imagem.src = `../src/images/front_${backgroundPadrao}`;
     imagem2.src = `../src/images/back_${backgroundPadrao}`;
 
-    // Desativar o flip-card
     flipCard.style.pointerEvents = 'none';
-    // Tornar a div do flip-card invisível
     flipCard.style.display = 'none';
 });
